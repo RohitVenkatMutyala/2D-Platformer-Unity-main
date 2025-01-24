a1cb6d55b36b4481879c95e0a5311ddce6b2d473
RohitVenkatMutyala <rohithvenkatmutyala@gmail.com>
2025-01-24 18:23:53 +0530

    connected the scenes and resoved some of the bugs and added music to the game

---
+++ Assets/sec2.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sec2 : MonoBehaviour
+{
+    //public Animator anim;
+    public PlaySound playSound;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //StartCoroutine("LevelExit");
+           
+             SceneManager.LoadScene(7);
+           
+        }
+    }
+
+    IEnumerator LevelExit()
+    {
+        //anim.SetTrigger("Exit");
+        yield return new WaitForSeconds(0.1f);
+
+        UIManager.instance.fadeToBlack = true;
+
+        yield return new WaitForSeconds(2f);
+        // Do something after flag anim
+        GameManager.instance.LevelComplete();
+    }
+}
